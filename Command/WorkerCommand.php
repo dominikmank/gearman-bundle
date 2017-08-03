03b75afce881f30904806f1f096c229b8bb00266
Dominik Mank <dominik@mank-online.de>
2017-08-03 12:09:36 +0200

    argument replaced by option

--- Command/WorkerCommand.php
+++ Command/WorkerCommand.php
@@ -8,6 +8,7 @@ use dmank\gearman\Worker;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class WorkerCommand extends ContainerAwareCommand
@@ -17,10 +18,10 @@ class WorkerCommand extends ContainerAwareCommand
         $this->setName('gearman:worker:run')
             ->setAliases(['run:gearman'])
             ->setDefinition([
-                new InputArgument('jobrepository', InputArgument::OPTIONAL, 'from which repository should the worker get his jobs?', 'default'),
-                new InputArgument('server', InputArgument::OPTIONAL, 'should the worker get his jobs from a specific server?', 'default'),
-                new InputArgument('memory_limit', InputArgument::OPTIONAL, 'how much memory can the worker use before restarting?', ini_get('memory_limit')),
-                new InputArgument('time_limit', InputArgument::OPTIONAL, 'how long should the worker run before restarting?', '+24 hours'),
+                new InputOption('jobrepository', 'r',InputOption::VALUE_OPTIONAL, 'from which repository should the worker get his jobs?', 'default'),
+                new InputOption('server', 's', InputOption::VALUE_OPTIONAL, 'should the worker get his jobs from a specific server?', 'default'),
+                new InputOption('memory_limit', 'ml', InputOption::VALUE_OPTIONAL, 'how much memory can the worker use before restarting?', ini_get('memory_limit')),
+                new InputOption('time_limit','tl',  InputOption::VALUE_OPTIONAL, 'how long should the worker run before restarting?', '+24 hours'),
             ]);
     }
 
@@ -29,20 +30,20 @@ class WorkerCommand extends ContainerAwareCommand
         $serverCollection = $this->getContainer()->get('gearman.server_collection');
         $jobRepository = $this->getContainer()->get('gearman.jobrepository');
 
-        if ($input->getArgument('server') != 'default') {
-            $connection = $this->getContainer()->get(sprintf('gearman.server.%s', $input->getArgument('server')));
+        if ($input->getOption('server') != 'default') {
+            $connection = $this->getContainer()->get(sprintf('gearman.server.%s', $input->getOption('server')));
             $serverCollection = new ServerCollection();
             $serverCollection->add($connection);
         }
 
-        if ($input->getArgument('jobrepository') != 'default') {
-            $jobRepository = $this->getContainer()->get(sprintf('gearman.jobrepository.%s', $input->getArgument('jobrepository')));
+        if ($input->getOption('jobrepository') != 'default') {
+            $jobRepository = $this->getContainer()->get(sprintf('gearman.jobrepository.%s', $input->getOption('jobrepository')));
         }
 
         $this->getContainer()->get('gearman.event_dispatcher')
-            ->addSubscriber(new MemoryLimit($input->getArgument('memory_limit'), $this->getContainer()->get('monolog.logger.gearman')));
+            ->addSubscriber(new MemoryLimit($input->getOption('memory_limit'), $this->getContainer()->get('monolog.logger.gearman')));
         $this->getContainer()->get('gearman.event_dispatcher')
-            ->addSubscriber(new MaxRuntime($input->getArgument('time_limit'), $this->getContainer()->get('monolog.logger.gearman')));
+            ->addSubscriber(new MaxRuntime($input->getOption('time_limit'), $this->getContainer()->get('monolog.logger.gearman')));
 
         $worker = new Worker($serverCollection, $jobRepository, $this->getContainer()->get('gearman.event_dispatcher'));
         $worker->run();
