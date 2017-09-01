22e60e85cf7e2be5c59b0354360d3879580dffa5
Carsten Leiendecker <cl@die-da.com>
2017-09-01 13:22:24 +0200

    No Comment!

--- Service/DispatcherInterface.php
+++ Service/DispatcherInterface.php
@@ -7,6 +7,6 @@ interface DispatcherInterface
     const PRIORITY_NORMAL = 1;
     const PRIORITY_HIGH = 2;
 
-    public function direct($jobName, $workLoad, $priority = DisptacherInterface::PRIORITY_LOW);
-    public function inBackground($jobName, $workLoad, $priority = DisptacherInterface::PRIORITY_LOW);
+    public function direct($jobName, $workLoad, $priority = self::PRIORITY_LOW);
+    public function inBackground($jobName, $workLoad, $priority = self::PRIORITY_LOW);
 }
